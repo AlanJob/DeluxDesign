6feb15d5bd9dd9683acc941a86f7c5dc5cc9ba79
Alan <alan.algoritmo@outlook.com>
2020-07-20 00:32:06 -0500

    Corregimos contador de promocion
    dejamos de ocupar libreria y creamos un código javascript donde el tiempo de promición es de una hora

--- js/countdown.js
+++ js/countdown.js
@@ -1,38 +1,34 @@
-var da, y, mes, d, h, mins, sec;
-da = new Date();
-y = da.getFullYear;
-mes = da.getMonth;
-d = da.getDay;
-h = da.getHours + 1;
-mins = da.getMinutes;
-sec = da.getSeconds;
-simplyCountdown('#reloj', {
-	year: 2020, // required
-	month: 7, // required
-	day: 18, // required
-	hours: 10, // Default is 0 [0-23] integer
-	minutes: 41, // Default is 0 [0-59] integer
-	seconds: 0, // Default is 0 [0-59] integer
-	words: { //words displayed into the countdown
-		days: 'D',
-		hours: 'H',
-		minutes: 'M',
-		seconds: 'S',
-		pluralLetter: 's'
-	},
-	plural: false, //use plurals
-	inline: true, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
-	inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
-	// in case of inline set to false
-	enableUtc: true, //Use UTC as default
-	onEnd: function() {
-		document.getElementById('bannerPromo').classList.add('oculta');
-		return; 
-	}, //Callback on countdown end, put your own function here
-	refresh: 1000, // default refresh every 1s
-	sectionClass: 'simply-section', //section css class
-	amountClass: 'simply-amount', // amount css class
-	wordClass: 'simply-word', // word css class
-	zeroPad: false,
-	countUp: false
-});
+const getRemainingTime = deadline => {
+	let now = new Date(),
+		remainTime = (new Date(deadline) - now + 1000) / 1000,
+		remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
+		remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
+		remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
+		remainDays = Math.floor(remainTime / (3600 * 24));
+  
+	return {
+	  remainSeconds,
+	  remainMinutes,
+	  remainHours,
+	  remainDays,
+	  remainTime
+	}
+  };
+  
+const countdown = (deadline,elem,finalMessage) => {
+	const el = document.getElementById(elem);
+  
+	const timerUpdate = setInterval( () => {
+	  let t = getRemainingTime(deadline);
+	  el.innerHTML = `${t.remainDays}d:${t.remainHours}h:${t.remainMinutes}m:${t.remainSeconds}s`;
+  
+	  if(t.remainTime <= 1) {
+		clearInterval(timerUpdate);
+		el.innerHTML = finalMessage;
+	  }
+  
+	}, 1000)
+  };
+  
+countdown(new Date().setHours(new Date().getHours()+1), 'clock', '¡Ya empezó!');
+  
