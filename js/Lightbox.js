73c276b6596e833a3976c0c53c0c13f2af8704cd
Alan <alan.algoritmo@outlook.com>
2020-08-21 01:17:46 -0500

    Product Modal
    Agregamos un modal para mejorar la experiencia de usuario al seleccionar algun  producto de la galeria

--- js/Lightbox.js
+++ js/Lightbox.js
@@ -1,4 +1,4 @@
-// MDB Lightbox Init
-$(function () {
-  $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+// Material Select Initialization
+$(document).ready(function() {
+  $('.mdb-select').materialSelect();
   });
